8d408f6b70a624e27a27c36490e10a6802e28e15
KhaLeak <124004316+KhaLeak@users.noreply.github.com>
2023-05-15 21:53:29 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+  int i;
+	int pid;
+  printf("pid : %d \n", pid);
+  pid = fork();
+  printf("pid : %d \n", pid);
+  printf("\n");
+
+  if (pid == 0) {
+    printf("Child starts\n");
+    for (i = 0; i < 5; i++) {
+      printf("i : %d\n", i);
+      printf("\n");
+    }
+    printf("Child ends\n");
+  }
+  else {
+    printf("\nBefore wait\n");
+    printf("getpid : %d\n", getpid());
+    pid = wait(0);
+    printf("\npid : %d\n", pid);
+    printf("After wait\n");
+    printf("Parent\n");
+    printf("getpid : %d\n", getpid());
+  }
+}
